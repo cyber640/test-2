770157199d0ddd3e18dcacf731a5548ac298c668
CodeFox <sebrinam394@gmail.com>
2025-05-12 19:27:22 +0300

    Add files via upload

---
+++ # Day2_Linux for users.py
@@ -0,0 +1,32 @@
+# Day2_Linux for users
+Catagory : Linux
+1) Information Gathering
+tools : nmap, netdiscover, whois, dig, nslookup, host, traceroute, curl, wget, ping, ifconfig, ipconfig, netstat, route, arp
+2) Vulnerability Scanning
+tools : nmap, nikto, gobuster, dirb, sqlmap, w3af, openvas, nessus, metasploit
+3) Exploitation
+tools : metasploit, sqlmap, hydra, medusa, john the ripper, aircrack-ng, burp suite, nikto, gobuster, dir
+4) Post Exploitation
+tools : metasploit, netcat, socat, msfvenom, powershell empire, mimikatz, hashcat, aircrack-ng, burp suite
+5) Web Application Attacks
+tools : burp suite, sqlmap, nikto, gobuster, dirb, w3af, zaproxy, metasploit, aircrack-ng
+6) Wireless Attacks
+tools : aircrack-ng, reaver, pixiewps, wifite, kismet, fern wifi cracker, wifiphisher, fluxion
+7) Data base Attacks
+tools : sqlmap, burp suite, metasploit, w3af, zaproxy, nikto, gobuster, dirb
+8) Password Attacks
+tools : hash Cat, john the ripper, hydra, medusa, aircrack-ng, burp suite, metasploit, w3af, zaproxy
+9) Social Engineering Attacks
+tools : social engineering toolkit, SET, metasploit, w3af, zaproxy, nikto, gobuster, dirb
+10) Sniffing and Spoofing
+tools : wireshark, tcpdump, ettercap, arpspoof, dsniff, aircrack-ng, burp suite, metasploit, w3af, zaproxy
+11) Forensics
+tools : sleuth kit, autopsy, volatility, binwalk, foremost, scalpel, ddrescue, photorec, testdisk, wireshark
+12) Reporting
+tools : record my desktop
+# Shells; communicate the user with the kernel
+  - in linux is called terminal
+In linux there is two users; Root users & Local(normal) User
+  1) Root user; has full permission
+   2) Local user; less permission than root user
+
